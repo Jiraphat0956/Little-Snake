c2bc1604579040bd13f58445657196d32ede24b2
Jiraphat0956 <115990714+Jiraphat0956@users.noreply.github.com>
2026-03-14 11:13:57 +0700

    CREATE : Grid System
    - Create Debug Grid In Scene
    - Add Check IsInsideGrid Function

---
+++ Assets/1_Project Folder/Scripts/Grid System.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+    [SerializeField] Vector2Int maxGridPosition;
+    [SerializeField] Vector2Int minGridPosition;
+
+    [SerializeField] bool showDebugGrid;
+
+    public bool IsInsideGrid(Vector2 position)
+    {
+        return (position.x >= minGridPosition.x && position.x <= maxGridPosition.x)
+            && (position.y >= minGridPosition.y && position.y <= maxGridPosition.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!showDebugGrid) return;
+        for(int i = minGridPosition.y;i < maxGridPosition.y; i++)
+        {
+            for(int j = minGridPosition.x; j < maxGridPosition.x; j++)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawSphere(new Vector3(j, i, 0), .5f);
+            }
+        }
+    }
+}
