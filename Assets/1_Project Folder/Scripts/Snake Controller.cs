c9678c3d5ebaa369571d505fe7fb5727de8d1680
Jiraphat0956 <115990714+Jiraphat0956@users.noreply.github.com>
2026-03-14 10:51:13 +0700

    UPDATE : Snake Controller
    - Make Snake Can Grow
    - Block Move Backward Direction

--- Assets/1_Project Folder/Scripts/Snake Controller.cs
+++ Assets/1_Project Folder/Scripts/Snake Controller.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SnakeController : MonoBehaviour
@@ -11,16 +12,32 @@ public class SnakeController : MonoBehaviour
     void Start()
     {
         InputManager.OnMove += Move;
+        InputManager.OnEscape += Grow;
     }
 
     void Move(Vector2 direction)
     {
+        Transform firstSegment = segmentList.First();
+
+        if (direction == -currentDirection) return;
         currentDirection = direction;
         for (int i = segmentList.Count - 1; i > 0; i--)
         {
             segmentList[i].position = segmentList[i - 1].position;
+            segmentList[i].rotation = segmentList[i - 1].rotation;
         }
-        transform.position = new Vector2(transform.position.x + direction.x, transform.position.y + direction.y);
-        transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg);
+        firstSegment.position = new Vector2(firstSegment.position.x + direction.x, firstSegment.position.y + direction.y);
+        firstSegment.eulerAngles = new Vector3(0, 0, Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg);
+    }
+
+    void Grow()
+    {
+        Transform lastSegment = segmentList.Last();
+
+        Vector2 newSegmentPosition = lastSegment.position - lastSegment.TransformDirection(Vector2.up);
+
+        GameObject newSegment = Instantiate(segmentPrefab, newSegmentPosition, lastSegment.rotation);
+        newSegment.transform.SetParent(transform);
+        segmentList.Add(newSegment.transform);
     }
 }
